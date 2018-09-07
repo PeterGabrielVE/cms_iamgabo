22201e3058658653a96be574d8c249201b673016
PeterGabrielVE <gaboleal123@gmail.com>
2018-09-07 10:41:02 -0430

    Vista para iniciar sesion del admin

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<?php require '../res/php/app_top_admin.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Administrador</title>
+
+    <link rel="stylesheet" href="../res/css/framework/semantic/semantic.min.css">
+    <link rel="stylesheet" href="../res/css/main.css">
+</head>
+<body>
+
+    <?php 
+        if(
+        isset($_SESSION['admin'])
+        ){
+        require '../views/nav/main_nav.php'; 
+        }
+    ?>
+
+    <?php 
+        if(
+            !isset($_SESSION['admin'])){
+            require '../views/admin/home.php';
+        }
+    ?>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  
+  <script src="../res/css/framework/semantic/semantic.min.js"></script>  
+</body>
+</html>
---
+++ views/admin/home.php
@@ -0,0 +1,18 @@
+<div class="login_container">
+
+<h2> Party Agency</h2>
+    <p ><b>Correo Electronico/Usuario</b></p>
+    <div class="ui input">
+        <input type="text" class="txtEmailLogin" placeholder="Correo Electronico/Usuario">
+    </div>  
+    <p ><b>Contrase&ntilde;a</b></p>
+    <div class="ui input">
+        <input type="password" class="txtPassLogin" placeholder="Contrase&ntilde;a">
+    </div>   
+
+    <div class="ui basic blue button">
+        Log In
+    </div>
+
+
+</div>
